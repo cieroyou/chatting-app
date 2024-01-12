6a862161f125416eac1cc193ff2d8dfe02f37de6
cieroyou <cieroyou1@gmail.com>
2024-01-12 10:35:07 +0900

    feat: 웹소켓 서버에 ws://localhost:8080/ws 로 연결 접속, 연결된 클라이언트가 서버로 text message 를 보내면 서버에서 Hello Client! 로 응답하는 간단한 소켓 메세징 프로그램 가능

---
+++ src/main/java/com/sera/chatting/WebSocketHandler.java
@@ -0,0 +1,36 @@
+package com.sera.chatting;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * 다수의 클라이언트의 메세지를 처리하는 핸들러
+ * 채팅에서는 텍스트 기반의 메세지가 오고 가므로 TextWebSocketHandler 를 상속받는다.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("afterConnectionEstablished: {}", session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, @NotNull CloseStatus status) throws Exception {
+        log.info("afterConnectionClosed: {}", session);
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws Exception {
+        log.info("handleTextMessage: {}", textMessage.getPayload());
+        session.sendMessage(new TextMessage("Hello Client!"));
+    }
+}
--- src/main/java/com/sera/chatting/config/WebSocketConfig.java
+++ src/main/java/com/sera/chatting/config/WebSocketConfig.java
@@ -1,15 +1,27 @@
 package com.sera.chatting.config;
 
+import com.sera.chatting.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+/**
+ * WebSocket을 활성화하기 위한 Config
+ *
+ * @EnableWebSocket 어노테이션을 사용하여 WebSocket 활성화
+ * ws://localhost:8080/ws Endpoint 로 커넥션 연결 가능
+ */
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
     }
+
 }
