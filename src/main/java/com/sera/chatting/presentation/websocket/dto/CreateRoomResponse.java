e0675803060e5e9cad4e69ceeef062e610259262
cieroyou <cieroyou1@gmail.com>
2024-07-15 16:23:04 +0900

    refactor: CreateRoomResponse class 에서 record로 변경

--- src/main/java/com/sera/chatting/presentation/websocket/dto/CreateRoomResponse.java
+++ src/main/java/com/sera/chatting/presentation/websocket/dto/CreateRoomResponse.java
@@ -1,16 +1,6 @@
 package com.sera.chatting.presentation.websocket.dto;
 
-import static lombok.AccessLevel.*;
-
 import com.sera.chatting.presentation.websocket.dto.common.AckData;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor(access = PRIVATE)
-@Getter
-public class CreateRoomResponse implements AckData {
-	String roomId;
+public record CreateRoomResponse(String roomId) implements AckData {
 }
